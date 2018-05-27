e149615b6e79cf72b1ce23a9fd1967eda94060be
GreatStephen <32407317+GreatStephen@users.noreply.github.com>
2018-05-28 00:34:50 +0800

    start

--- SecondHandPlatform/app/src/main/java/com/example/lty/secondhandplatform/MainActivity.java
+++ SecondHandPlatform/app/src/main/java/com/example/lty/secondhandplatform/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lty.secondhandplatform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        click();
+    }
+
+    private void click(){
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, hall.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ SecondHandPlatform/app/src/main/java/com/example/lty/secondhandplatform/hall.java
@@ -0,0 +1,20 @@
+package com.example.lty.secondhandplatform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LTY on 2018/5/28.
+ */
+
+public class hall extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hall);
+    }
+
+
+
+}
