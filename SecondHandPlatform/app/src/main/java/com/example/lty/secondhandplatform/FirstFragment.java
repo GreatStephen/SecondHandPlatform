16bb377267ee9ce5c877174c52c9d279cb636539
GreatStephen <601269035@qq.com>
2018-05-28 09:47:13 +0800

    hall

---
+++ SecondHandPlatform/app/src/main/java/com/example/lty/secondhandplatform/FirstFragment.java
@@ -0,0 +1,20 @@
+package com.example.lty.secondhandplatform;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by STEPHEN on 2018/5/28.
+ */
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg1, container, false);
+        return view;
+    }
+}
---
+++ SecondHandPlatform/app/src/main/java/com/example/lty/secondhandplatform/FourthFragment.java
@@ -0,0 +1,19 @@
+package com.example.lty.secondhandplatform;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by STEPHEN on 2018/5/28.
+ */
+
+public class FourthFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg4, container, false);
+        return view;
+    }
+}
